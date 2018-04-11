58014e14b61562f24c6e373dc58ddb608906b5bf
albertob <alberto.bonora@gmail.com>
2018-04-10 22:31:06 -0400

    Nav Work
    Set up sass files for nav, variables and resets. wired up component test
    on about view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <header>
+      <!-- LOGO WILL GO HERE -->
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="about">About</router-link>
+      </nav>
+    </header>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -11,13 +18,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/scss/main.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
